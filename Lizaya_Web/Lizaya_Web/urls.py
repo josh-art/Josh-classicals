4b2c80ec68eef14d211d19a06363a0e7ce77eabc
josh-art <56514140+josh-art@users.noreply.github.com>
2024-03-09 15:41:22 +0300

    Update urls.py

--- Lizaya_Web/Lizaya_Web/urls.py
+++ Lizaya_Web/Lizaya_Web/urls.py
@@ -16,17 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
-from django.conf.urls import url
-from django.views.static import serve
 from django.conf.urls.static import static
 from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Lizaya_app.urls')),
-
-    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
