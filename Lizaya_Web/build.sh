a92700904ab6baa9fd7da7668f80d2c9bc0a468d
josh-art <56514140+josh-art@users.noreply.github.com>
2024-03-09 12:46:25 +0300

    Create build.sh

---
+++ Lizaya_Web/build.sh
@@ -0,0 +1,12 @@
+#/bin/bsh
+
+#build the project
+echo "building the project..."
+python3.9 -m pip install -r requirements.txt
+
+echo "make migrations..."
+python3.9 manage.py makemigrations ==noinput
+python3.9 manage.py migrate ==noinput
+
+echo "collect static..."
+python3.9 manage.py collectstatic ==noinput ==clear
