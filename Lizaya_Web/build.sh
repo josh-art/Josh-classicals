7c707ca467f3cf5b499a5a2f28ca4c399488eb2f
josh-art <56514140+josh-art@users.noreply.github.com>
2024-03-09 15:53:20 +0300

    Update build.sh

--- Lizaya_Web/build.sh
+++ Lizaya_Web/build.sh
@@ -5,8 +5,8 @@ echo "building the project..."
 python3.9 -m pip install -r requirements.txt
 
 echo "make migrations..."
-python3.9 manage.py makemigrations ==noinput
-python3.9 manage.py migrate ==noinput
+python3.9 manage.py makemigrations 
+python3.9 manage.py migrate 
 
 echo "collect static..."
-python3.9 manage.py collectstatic ==noinput
+python3.9 manage.py collectstatic 
