eeba8f745dce7a27a7546ecb77de55a7078ebb91
josh-art <56514140+josh-art@users.noreply.github.com>
2024-03-11 12:43:03 +0300

    Update settings.py

--- Lizaya_Web/Lizaya_Web/settings.py
+++ Lizaya_Web/Lizaya_Web/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 SITE_ID = 1
