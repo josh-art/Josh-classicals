d0beaa21787ae13814c674cc0a7354b308ea0e94
josh-art <56514140+josh-art@users.noreply.github.com>
2024-03-11 12:09:26 +0300

    Update settings.py

--- Lizaya_Web/Lizaya_Web/settings.py
+++ Lizaya_Web/Lizaya_Web/settings.py
@@ -133,10 +133,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', "static")
+
+
 SITE_ID = 1
 # LOGIN_REDIRECT_URL = '/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static"),
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
