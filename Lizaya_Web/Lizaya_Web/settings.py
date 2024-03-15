fbc377f218e1f21a0d4a2743c3e31b4a89e9ec49
josh-art <56514140+josh-art@users.noreply.github.com>
2024-03-16 02:22:46 +0300

    'static'

--- Lizaya_Web/Lizaya_Web/settings.py
+++ Lizaya_Web/Lizaya_Web/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR , 'staticfiles_build'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 SITE_ID = 1
