d304125227f9a94c801dbfe10a92d89d5f44aefc
josh-art <56514140+josh-art@users.noreply.github.com>
2024-03-16 09:32:38 +0300

    'collect'

--- Lizaya_Web/build.sh
+++ Lizaya_Web/build.sh
@@ -6,5 +6,8 @@ python3.9 -m pip install -r requirements.txt
 
 echo "make migrations..."
 python3.9 manage.py makemigrations 
-python3.9 manage.py migrate 
+python3.9 manage.py migrate
+
+echo "collect static"
+python3.9 manage.py collectstatic
 
