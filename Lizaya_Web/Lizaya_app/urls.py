191ac6ecbd31c7ccce33a78bdc8041800ac189d5
josh-art <56514140+josh-art@users.noreply.github.com>
2024-03-31 15:20:55 +0300

    Update urls.py

--- Lizaya_Web/Lizaya_app/urls.py
+++ Lizaya_Web/Lizaya_app/urls.py
@@ -7,8 +7,8 @@ from .views import PostCreateView
 
 
 urlpatterns = [
-    path('', views.error, name='error'),
-    path('home', views.index1, name='index'),
+    path('error', views.error, name='error'),
+    path('', views.index1, name='index'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post_update', views.post_update, name='post_update'),
     path('feedback/', views.contactForm, name='feedback'),
